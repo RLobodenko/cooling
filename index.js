c30e54c20b5b965122f0417d69c5c2750d18ae2f
Roman <47541355+RLobodenko@users.noreply.github.com>
2026-06-01 20:08:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+
+var one = document.getElementById('1');
+
+
+
+
+
+function f1(){
+    
+    var a = document.getElementById('in7').value;
+    var b = document.getElementById('in8').value;
+      
+  var c = document.getElementById('in9').value;
+     var d = document.getElementById('in10').value;
+ var m = document.getElementById('in6').value;
+     
+    
+    var e = 345.96 * m + 676 * a + 729 * b + 2500 * c + 2641.96 * d;
+
+var f = 18.6 * m + 26 * a + 27 * b + 50 * c + 51.4 * d;
+    
+    var j = e / f ;
+
+ 
+            var k = 20 / j;
+            document.getElementById('out2').innerHTML = k.toFixed(2) + " кПа";
+    
+    var l = 480 / j
+    document.getElementById('out3').innerHTML = l.toFixed(2) + " кПа";
+       
+    }
+ 
+  
+
+     
+   
+    
+
+
+
+
